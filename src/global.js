ff773034ba5a68934fd80eaa707db2aea03fb4e5
PlattDominic <74417360+PlattDominic@users.noreply.github.com>
2023-03-21 13:59:05 -0400

    Started second video on course

---
+++ src/global.js
@@ -0,0 +1,20 @@
+// The Global Object!!!
+
+// console.log(global);
+
+let number = 0;
+
+// let sayHello = () => console.log('hi');
+// let addNumbers = () => {
+//     console.log(number++ + number);
+//     if (number > 5) clearInterval(add);
+// }
+
+// // global.setTimeout(sayHello, 2000);
+// setTimeout(sayHello, 2000);
+// // setInterval just calls a function every (delay) ms
+// let add = setInterval(addNumbers, 1000);
+
+
+console.log(__dirname);
+console.log(__filename)
